5847760e1f6ffe029d4fa891ae9bfd5669730ec5
abpandanguyen <abpandanguyen@gmail.com>
2022-08-08 11:08:35 -0700

    Part 2 Done

--- main_app/models.py
+++ main_app/models.py
@@ -1,3 +1,13 @@
+from smtplib import SMTPRecipientsRefused
 from django.db import models
 
 # Create your models here.
+
+class Finch(models.Model):
+    commonName = models.CharField(max_length=100)
+    species = models.CharField(max_length=100)
+    description = models.TextField(max_length=250)
+    iocSequence = models.IntegerField()
+
+    def __str__(self):
+        return f'{self.commonName} ({self.id})'
--- main_app/views.py
+++ main_app/views.py
@@ -1,27 +1,20 @@
 from django.shortcuts import render
+from .models import Finch
 
 from django.http import HttpResponse
 
-class Finch:
-    def __init__(self, commonName, species, description, iocSequence):
-        self.commonName = commonName
-        self.species = species
-        self.description = description
-        self.iocSequence = iocSequence
 
-finches = [
-    Finch('Common chaffinch', 'Fringilla coelebs', 'Common and widespread bird in the finch family.', 1),
-    Finch('Brambling', 'Fringilla montifringilla', 'Also known as the "cock of the north".', 4),
-    Finch('Pine grosbeak', 'Pinicola enucleator', 'Found in coniferous woods, name translating directly to "bird that lives in the pines and shells the seeds".', 14),
-    Finch('Scarlet finch', 'Carpodacus sipahi', 'Found in hte Himalayas and across Nepal stretching as far south as Thailand.', 38),
-    Finch('Palila', 'Loxioides bailleui', 'Critically endangered. Golden-yellow head and breast with a light belly, gray back, and greenish wings and tail.', 72),
-]
 # Define the home view
 def home(request):
-    return HttpResponse('<h1>Welcome to the Finch Collection!</h1>')
+    return render(request, 'home.html')
 
 def about(request):
     return render(request, 'about.html')
 
 def finches_index(request):
+    finches = Finch.objects.all()
     return render(request, 'finches/index.html', { 'finches': finches })
+
+def finches_detail(request, finch_id):
+    finch = Finch.objects.get(id=finch_id)
+    return render(request, 'finches/detail.html', { 'finch': finch })
